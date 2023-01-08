cdce45d87a054bd50dfdf0f329e5775e95f48868
Jaidiver Gómez <jaidiver.gomez@experimentality.co>
2023-01-08 13:19:28 -0500

    feat: collections with angular cdk.

--- src/app/collections/collections.component.ts
+++ src/app/collections/collections.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-collections',
   templateUrl: './collections.component.html',
-  styleUrls: ['./collections.component.scss']
+  styleUrls: ['./collections.component.scss'],
 })
-export class CollectionsComponent implements OnInit {
+export class CollectionsComponent {
+  selection = new SelectionModel<any>(
+    true, // multiple selection or not
+    [] // initial selected values
+  );
 
-  constructor() { }
+  optionList = [
+    { id: 1, title: 'Hacer ejemplo' },
+    { id: 2, title: 'Grabar video' },
+    { id: 3, title: 'Editar video' },
+    { id: 4, title: 'Hacer miniatura' },
+    { id: 5, title: 'Subir video' },
+  ];
 
-  ngOnInit() {
+
+  get allSelected(): boolean {
+    return (
+      !!this.optionList.length &&
+      this.selection.selected.length === this.optionList.length
+    );
   }
 
+  toggleMasterSelection() {
+    if (this.allSelected) {
+      this.selection.clear();
+    } else {
+      this.selection.select(...this.optionList);
+    }
+  }
 }
